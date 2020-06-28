9c77a6d112cb4257841f655962e4fbfcf1eda1ff
W Laptop <assimil1999@gmail.com>
2020-06-28 17:30:18 +0200

    Basis für die Gallerie

--- GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/gallery/GalleryFragment.java
+++ GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/gallery/GalleryFragment.java
@@ -1,9 +1,13 @@
 package com.example.geoapp2020.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,22 +18,30 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.geoapp2020.R;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements OnItemClickListener {
 
     private GalleryViewModel galleryViewModel;
+    ImageAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+        GridView galerie = (GridView) root.findViewById(R.id.grid_view);
+        adapter = new ImageAdapter(getContext());
+        galerie.setAdapter(adapter);
+        galerie.setOnItemClickListener(this);
+
         return root;
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+        int resid = (Integer) adapter.getItem(pos);
+        Intent intent = new Intent(getContext(), SingleImage.class);
+        intent.putExtra("resId", resid);
+        startActivity(intent);
     }
 }
---
+++ GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/gallery/ImageAdapter.java
@@ -0,0 +1,50 @@
+package com.example.geoapp2020.ui.gallery;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.example.geoapp2020.R;
+
+public class ImageAdapter  extends BaseAdapter {
+    private Context context;
+    private Integer[] bilderIDs = {
+            R.raw.image_01, R.raw.image_02,
+            R.raw.image_03, R.raw.image_04,
+            R.raw.image_05, R.raw.image_06,
+            R.raw.image_07, R.raw.image_08,
+            R.raw.image_09, R.raw.image_10,
+            R.raw.image_11, R.raw.image_12
+    };
+
+    public ImageAdapter(Context c) {
+        context = c;
+    }
+
+    @Override
+    public int getCount() { return bilderIDs.length; }
+
+    @Override
+    public Object getItem(int position) { return bilderIDs[position]; }
+
+    @Override
+    public long getItemId(int position) { return 0; }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView iv;
+        if(convertView == null){
+            iv = new ImageView(context);
+            iv.setLayoutParams(new GridView.LayoutParams(300, 300));
+            iv.setPadding(5,5,5,5);
+            iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        }else {
+            iv = (ImageView) convertView;
+        }
+        iv.setImageResource(bilderIDs[position]);
+        return iv;
+    }
+}
