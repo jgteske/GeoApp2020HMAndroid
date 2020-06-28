b000bcc297a2c0a5a7e0ad721ab8e9d9bf192c70
W Laptop <assimil1999@gmail.com>
2020-06-28 21:01:02 +0200

    Basis Home Buttons

--- GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/home/HomeFragment.java
+++ GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.geoapp2020.R;
 
@@ -24,6 +26,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -32,8 +35,18 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        // setting home screen titleimage
         ImageView imageView = (ImageView) root.findViewById(R.id.title_image);
         imageView.setImageResource(R.drawable.title_image);
+
+        // setting up buttons
+        Button button = (Button) root.findViewById(R.id.button_aufgaben);
+        button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_webview, null));
+        Button button1 = (Button) root.findViewById(R.id.button_gallery);
+        button1.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_gallery, null));
+        Button button2 = (Button) root.findViewById(R.id.button_map);
+        button2.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_map, null));
+
         return root;
     }
 }
