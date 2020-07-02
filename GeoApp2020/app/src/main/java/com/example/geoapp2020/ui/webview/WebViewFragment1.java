3320af3707ebb6d953c5ff9f148711dfdd7b3050
W Laptop <assimil1999@gmail.com>
2020-07-02 23:22:06 +0200

    Hinzufügen zweier Untermenüpunkte für Webviews

---
+++ GeoApp2020/app/src/main/java/com/example/geoapp2020/ui/webview/WebViewFragment1.java
@@ -0,0 +1,46 @@
+package com.example.geoapp2020.ui.webview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.geoapp2020.R;
+
+public class WebViewFragment1 extends Fragment {
+
+    private WebViewModel webViewModel;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        webViewModel = ViewModelProviders.of(this).get(WebViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_webview, container, false);
+        // Header for the Webview
+
+    // Webview
+        // Load the Webview for the webview
+        final WebView webView = root.findViewById(R.id.webview);
+        webView.loadUrl("http://fk08srv01-2.geo.private.hm.edu/~teske/pages/klauer_aufgabe5.html");
+
+        // Activate the Javascrip
+        // source: https://stackoverflow.com/questions/29584597/android-studio-how-to-set-a-webview-loading-and-error-view
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // Force links and redirects to open in the WebView instead of in a browser
+        webView.setWebViewClient(new WebViewClient());
+
+        return root;
+    }
+}
+
+
+
+
